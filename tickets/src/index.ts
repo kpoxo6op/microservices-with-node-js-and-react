b51757c244ca6db7637cfd5c3e0d2de15c03b440
kpoxo6op <kpoxo6op@gmail.com>
2021-04-03 22:41:14 +1300

    312

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
@@ -8,6 +10,7 @@ const start = async () => {
     throw new Error('MONGO_URI must be defined');
   }
   try {
+    await natsWrapper.connect('ticketing', 'rhrhrhrh', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
